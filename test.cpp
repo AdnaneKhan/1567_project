25932bbd4e2fd873eedb45f6cacdf60c88dabca0
Adnan <adnankhan@gmail.com>
2014-11-17 21:54:52 -0500

    scaffold for test class

---
+++ test.cpp
@@ -0,0 +1,28 @@
+#define DEBUG
+
+#include "Locator.h"
+
+
+int main() {
+
+    // TEST AUDIO CONNECTOR
+
+
+
+    // TEST IMAGE PROCESSOR
+
+
+
+    // TEST ARDUINO CONNECTOR
+
+
+
+    // TEST GRAPH
+
+
+
+    // TEST NODE
+
+    return 0;
+}
+
