10d63c472c7b06a3eec9ff80f6d3632591b18503
Adnan <adnankhan@gmail.com>
2014-11-26 18:33:53 -0500

    updated main to be flexible in run configurations

--- main.cpp
+++ main.cpp
@@ -3,39 +3,60 @@
 
 #include "Locator.h"
 
+#define SIMULATION 2
+#define REAL 1
+#define INVALID 3
 
-int main() {
+typedef int operationType;
 
-    std::string init = "val";
-#ifdef __arm__
-        system("omxplayer -o local audio_prompts/left.mp3");
-        #else
-    system("afplay /Users/adnankhan/Box\\ Sync/Robots/1567_project/audio_prompts/begun.m4a");
-#endif
+operationType select_operation( int num_args,char * args[]) {
+    operationType to_return;
+    if (num_args == 4  && strncmp(args[1],"-R",2) == 0) {
+        to_return = SIMULATION;
+    } else if(num_args == 3 && strncmp(args[1],"-S",2) == 0 ) {
+        to_return = REAL;
+    } else {
+        to_return = INVALID;
+    }
 
-    Locator *loc = new Locator(" ", "/dev/tty.usbmodem1451");
+    return to_return;
+}
+
+/*
+ Reads images from the image folder (instead of camera) and data from a text file
+ with the path mock_data_file (instead of from the arduino.
+ */
+void run_simulation(const char * image_folder, const char * mock_data_file) {
+    
+}
+
+/*
+Runs the actual locater using data from camera and arduino serial connection
+ */
+void run_full(const char * serial_port) {
+    Locator * loc = new  Locator(" ", serial_port);
     std::chrono::milliseconds timespan(500);
-    int res = loc->start("/dev/tty.usbmodem1451");
+    int res = loc->start(serial_port);
+
     if (res) {
         while (!loc->is_located()) {
             loc->run_locator();
             std::this_thread::sleep_for(timespan);
         }
+    } else {
+        std::cout << "There was a problem with initializing the locator.\n";
     }
+}
 
-    delete(loc);
-//    Image_Processor proc = Image_Processor();
-//    Camera_Connector camera = Camera_Connector(USB_WEBCAM, "", 0);
-//    Mat img = camera.get_image();
-//
-//    proc.hough_rectangle_detect(img);
-//
+int main(int argc, char * argv[]) {
 
-//    int intersection;
-//
-//    int res = proc.step_detect(camera, intersection);
-//
-//    std::cout << " The result was " << intersection << std::endl;
+    if (select_operation(argc, argv) == REAL) {
+        run_full(argv[2]);
+    } else if (select_operation(argc, argv) == SIMULATION) {
+        run_simulation(argv[2], argv[3]);
+    } else {
+        std::cout << "Proper command arguments were not specified, exiting.\n";
+    }
 
     return 0;
 }
