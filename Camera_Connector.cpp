12fc57b26b67765081b0b8ca4f2ea923bd157281
Adnan <adnankhan@gmail.com>
2014-12-09 19:18:03 -0500

    longer sleep before camera

--- Camera_Connector.cpp
+++ Camera_Connector.cpp
@@ -76,7 +76,7 @@ void usb_camera_init(cv::VideoCapture &to_init, int camera_id) {
 
 #ifdef __arm__
 int pi_camera_init( raspicam::RaspiCam_Cv & to_init ) {
-    to_init.set(CV_CAP_PROP_FORMAT, CV_8UC1 );
+    to_init.set(CV_CAP_PROP_FORMAT, CV_8UC3 );
 
     if (!to_init.open()) {
         // Problem
